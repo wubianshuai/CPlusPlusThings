630b9ec8177e501b6595f61fc7862afcdb8aa40c
“zzy” <895649881@qq.com>
2025-02-13 19:14:01 +0800

    2025.2.13

--- test.cpp
+++ test.cpp
@@ -1,19 +1,11 @@
-#include <stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<iterator>
+using namespace std;
 int main()
 {
- 
-   // int x = 10;
-   // int *y = &x;
- 
-   // printf("x: %d Address of x: %d\n", x, &x);
-   // printf("y: %d \n", y);
-   // printf("Address of y: %d \n", &y);
-   // printf("Value at address in y: %d\n", *y);
-   // return 0;
+  
 
-  printf("*\n");
-  printf("***\n");
-  printf("*****\n");
-  printf("*******");
    
 }
